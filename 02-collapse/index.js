592090fd82bc5c0133bf0ca6b588ed3fbc940e33
Pavel <designdpcompany@gmail.com>
2022-08-13 11:51:31 +0300

    Button text fix

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,6 +1,18 @@
 const btn = document.querySelector('.collapsible__button');
 const content = document.querySelector('.collapsible__content');
+const btnContHide = document.querySelector('.collapsible__action--hidden');
+const btnContShow = document.querySelector('.collapsible__action--visible');
+btnContHide.style.display = "none";
 btn.addEventListener('click', function () {
+   btnContShow.style.display = "none";
+   if (btnContHide.style.display === "none") {
+      btnContHide.style.display = "inline";
+      btnContShow.style.display = "none";
+   } else {
+      btnContHide.style.display = "none";
+      btnContShow.style.display = "inline";
+   }
+   
    if (content.classList.contains('hidden')) {
       content.animate([{
             opacity: 0,
